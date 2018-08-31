a927f92a4c531949734c0c0b249f32300548cac0
yvesn <yves.noirjean@ynoir.ch>
2018-08-31 19:27:25 +0200

    added lombok and Rectangle model class

---
+++ src/main/java/ch/ynoir/intersect/model/Rectangle.java
@@ -0,0 +1,16 @@
+package ch.ynoir.intersect.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+public class Rectangle
+{
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+}
